81750dc751ecb302c45621bc5339ce346dd7473c
biyakim <w2116@e-mirim.hs.kr>
2022-11-29 18:33:12 +0900

    로그인 회원가입 만들기

---
+++ app/src/main/java/com/example/diary/SignUpActivity.kt
@@ -0,0 +1,16 @@
+package com.example.diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class SignUpActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/diary/loginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.diary
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class loginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
+
